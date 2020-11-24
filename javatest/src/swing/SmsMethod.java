ee663731c0f2c94e5a0f2d5354ac3911b5426b0c
chmh0805 <chmh0805@naver.com>
2020-11-24 12:14:15 +0900

    정민혁 201124 프로그래밍 언어활용

---
+++ javatest/src/swing/SmsMethod.java
@@ -0,0 +1,32 @@
+package swing;
+
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+public class SmsMethod {
+	
+	public static void 문자발송(String to, String text) {
+		String api_key = "NCSNA0TV7AG7WSRA";
+	    String api_secret = "DOLY8HYPOZVGGS6NPZKGIBNSB84NT8QY";
+	    Message coolsms = new Message(api_key, api_secret);
+
+	    // 4 params(to, from, type, text) are mandatory. must be filled
+	    HashMap<String, String> params = new HashMap<String, String>();
+	    params.put("to", to);
+	    params.put("from", "01065188188");
+	    params.put("type", "SMS");
+	    params.put("text", text);
+	    params.put("app_version", "test app 1.2"); // application name and version
+
+	    try {
+	      JSONObject obj = (JSONObject) coolsms.send(params);
+	      System.out.println(obj.toString());
+	    } catch (CoolsmsException e) {
+	      System.out.println(e.getMessage());
+	      System.out.println(e.getCode());
+	    }
+	}
+
+}
